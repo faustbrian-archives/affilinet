d7e3189488f633a47ab1be08b23b58614651c1f3
Brian Faust <faustbrian@users.noreply.github.com>
2016-11-30 10:09:37 +0200

    Apply fixes from StyleCI (#1)
    [ci skip] [skip ci]

--- tests/AbstractTestCase.php
+++ tests/AbstractTestCase.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of Affili.net PHP SDK.
+ *
+ * (c) Brian Faust <hello@brianfaust.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace BrianFaust\Tests\Affilinet;
 
 abstract class AbstractTestCase extends \PHPUnit_Framework_TestCase
